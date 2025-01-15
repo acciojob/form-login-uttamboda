c7f2cd1ac8b035072abd453c1b069f9c1dedeaa2
uttamboda <167513333+uttamboda@users.noreply.github.com>
2025-01-15 12:55:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
     //Write your code here
+	const form = document.getElementById("form1");
+
+    const firstName = form.elements["fname"].value;
+    const lastName = form.elements["lname"].value;
+
+    alert(`${firstName} ${lastName}`);
+
+    return false;
 
 }
